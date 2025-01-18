0d33af0a5fd065b9a69ce2c5bb0873ca83bfbbce
jellofsh <jellofsh@umich.edu>
2025-01-18 16:52:15 -0500

    c

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'adaptive-website' title`, () => {
+  it(`should have the 'adaptive-gymnastics' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('adaptive-website');
+    expect(app.title).toEqual('adaptive-gymnastics');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, adaptive-website');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, adaptive-gymnastics');
   });
 });
